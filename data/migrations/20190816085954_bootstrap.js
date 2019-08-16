2dacbec69b1915c080fee633d3dd4f5651091269
Dave Irwin <david@redirwin.com>
2019-08-16 09:28:20 -0700

    Created schema according to project instructions.

--- data/migrations/20190816085954_bootstrap.js
+++ data/migrations/20190816085954_bootstrap.js
@@ -1,8 +1,70 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("project", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 255)
+        .notNullable()
+        .unique();
+      tbl.string("description", 2000);
+      tbl
+        .boolean("completed")
+        .notNullable()
+        .defaultTo("false");
+    })
+    .createTable("task", tbl => {
+      tbl.increments();
+      tbl
+        .string("description", 2000)
+        .notNullable()
+        .unique();
+      tbl.string("notes", 5000);
+      tbl
+        .boolean("completed")
+        .notNullable()
+        .defaultTo("false");
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("project")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("resource", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 255)
+        .notNullable()
+        .unique();
+      tbl.string("description", 2000);
+    })
+    .createTable("project_resources", tbl => {
+      tbl.increments();
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("project")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resource")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("resource")
+    .dropTableIfExists("task")
+    .dropTableIfExists("project");
 };
